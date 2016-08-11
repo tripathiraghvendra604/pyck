e22c753387a05a72233736c0c88348b687575820
raghvendra tripathi <tripathiraghvendra604@gmail.com>
2016-08-12 01:43:28 +0530

    advanced models

--- api/models.py
+++ api/models.py
@@ -5,27 +5,23 @@ from django.db import models
 # Create your models here.
 
 
-class Info(models.Model):
+class Area(models.Model):
     pincode = models.IntegerField()
-
-    delhivery = models.CharField(max_length=5)
-    delhivery_limit = models.IntegerField()
-
-    fedex = models.CharField(max_length=5)
-    fedex_limit = models.IntegerField()
-
-    dotzot = models.CharField(max_length=5)
-    dotzot_limit = models.IntegerField()
-
-    dtdc = models.CharField(max_length=5)
-
-    indiapost = models.CharField(max_length=5)
-
-    pref1 = models.CharField(max_length=100)
-    pref2 = models.CharField(max_length=100)
-    pref3 = models.CharField(max_length=100)
-    pref4 = models.CharField(max_length=100)
-    pref5 = models.CharField(max_length=100)
 
     def __unicode__(self):
         return self.pincode
+
+class Company(models.Model):
+    company = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.company
+
+class Delivery(models.Model):
+    pincode = models.ForeignKey(Area)
+    company = models.ForeignKey(Company)
+    price = models.IntegerField()
+    prefrence = models.IntegerField()
+
+    class Meta:
+        ordering = ['-prefrence']
